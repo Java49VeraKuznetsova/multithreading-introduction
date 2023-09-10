377998b6cc7b8f9fdc69afe6dcc5bf2da5bfa11d
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-09-10 19:04:45 +0300

    HW#44 Yuri

--- multyThreading/src/telran/multithreading/Printer.java
+++ multyThreading/src/telran/multithreading/Printer.java
@@ -1,5 +1,6 @@
-package telran.multithreading;
 /*
+package telran.multithreading;
+
 public class Printer extends Thread {
 private char symbol;
 private int nSymbols;
@@ -22,6 +23,7 @@ public void run() {
 
 
 */
+/*
 public class Printer extends Thread {
 	private char []  symbols;
 	private boolean running = true;
@@ -51,4 +53,52 @@ public class Printer extends Thread {
 		}
 	}
 	
+}
+*/
+// HW 44
+package telran.multithreading;
+
+public class Printer extends Thread {
+	private static final long SLEEPING_TIME = 100;
+	int printerNumber;
+	Printer next;
+	private int inLine;
+	static int overall;
+	static int nPartitions;
+
+	public Printer(int printerNumber) {
+		this.printerNumber = printerNumber;
+		inLine = overall / nPartitions;
+	}
+
+	public void setNext(Printer next) {
+		this.next = next;
+	}
+
+	public static void setOverall(int overall) {
+		Printer.overall = overall;
+	}
+
+	public static void setPartitions(int nPartitions) {
+		Printer.nPartitions = nPartitions;
+
+	}
+
+	@Override
+	public void run() {
+		int count = 0;
+		String line = (" " + printerNumber).repeat(inLine);
+		while (count < nPartitions) {
+			try {
+				join();
+
+			} catch (InterruptedException e) {
+				System.out.println(line);
+				next.interrupt();
+				count++;
+			}
+		}
+
+	}
+
 }
--- multyThreading/src/telran/multithreading/PrinterControllerAppl.java
+++ multyThreading/src/telran/multithreading/PrinterControllerAppl.java
@@ -1,3 +1,4 @@
+/*
 package telran.multithreading;
 
 import java.time.Instant;
@@ -23,6 +24,7 @@ public class PrinterControllerAppl {
 
 }
 */
+/*
 public class PrinterControllerAppl {
 
 	public static void main(String[] args)   {
@@ -43,3 +45,40 @@ public class PrinterControllerAppl {
 		printer.stopPrinter();
 	}
 	}
+*/
+// HW 44 YURI
+package telran.multithreading;
+import java.lang.Thread.State;
+
+public class PrinterControllerAppl {
+
+	private static final int N_THREADS = 50;
+	private static final int N_NUMBERS = 100;
+	private static final int N_PARTITIONS = 10;
+	
+	
+
+	public static void main(String[] args) {
+		Printer.setPartitions(N_PARTITIONS);
+		Printer.setOverall(N_NUMBERS);
+		Printer[] printers=new Printer[N_THREADS];
+		creatingPrinters(printers);
+		
+		printers[0].interrupt();
+		
+	}
+
+	
+
+	private static void creatingPrinters(Printer[] printers) {
+		printers[0]=new Printer(1);
+		for(int i=1; i<printers.length; i++){
+			printers[i]=new Printer(i+1);
+			printers[i-1].setNext(printers[i]);
+			printers[i-1].start();
+		}
+		printers[printers.length-1].setNext(printers[0]);
+		printers[printers.length-1].start();
+	}
+
+}
