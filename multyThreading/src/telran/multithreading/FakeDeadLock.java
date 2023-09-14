84a00ad14d4849d9eea9ecea6f7defa40d4fcb95
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-09-14 19:07:18 +0300

    HW 97(46) - part2

--- multyThreading/src/telran/multithreading/FakeDeadLock.java
+++ multyThreading/src/telran/multithreading/FakeDeadLock.java
@@ -4,60 +4,35 @@ public class FakeDeadLock {
 
 	
 	private static final int PARAM = 10;
-	private static int value = 0;
 	private static Object mutex1 = new Object();
 	private static Object mutex2 = new Object();
 	
 	public static void main(String[] args) throws InterruptedException {
 		//Thread.currentThread().join();
-
-//		FakeDeadLock thread1 = new FakeDeadLock(PARAM);
-	//	FakeDeadLock thread2 = new FakeDeadLock(PARAM);
 		
 		Thread1 thread1= new Thread1();
 		Thread2 thread2= new Thread2();
 		thread1.start();
 		thread2.start();
-		/*
-	for (int i=0; i<2; i++) {
-		thread1();
-	thread2();
-	}
-	*/
-	//System.out.println(value);
-	}
-	/*
-	private static synchronized  void thread1() {
-		value++;
-		thread2();
+		
 	}
 	
-	private static void thread2() {
-		synchronized (mutex1) {
-			value++;
-			thread1();
-		}
 	
-	}
-	*/
 private static class Thread1 extends Thread {
 	@Override	
 	public void run() {
 			synchronized (mutex1) {
 				
-				//	System.out.println("thread1 - mutex1");
-					try {
+								try {
 					Thread.sleep(PARAM);
-					//	value+=1;
+					
 					} catch (InterruptedException e) {
 						
 						e.printStackTrace();
 					}
-				 				
-				//	System.out.println("thread1 - catch");
-					//*/
+				 
 								synchronized (mutex2) {
-					//System.out.println("thread1 - mutex2");
+				
 				}
 			}
 		}
@@ -68,15 +43,14 @@ private static class Thread2 extends Thread {
 		synchronized (mutex2) {
 			
 			try {
-			//	System.out.println("thread2 - mutex2");
+		
 				Thread.sleep(PARAM);
+				
 			} catch (InterruptedException e) {
 				
 				e.printStackTrace();
 			}
-		//	*/
-			//value+=1;
-			//System.out.println("thread2 - catch");
+	
 			synchronized (mutex1) {
 				
 			}
