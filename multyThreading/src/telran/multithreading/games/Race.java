11efdc909438cdba02d791d4ffdbb10f63d29472
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-09-19 19:58:24 +0300

    HW 46(97) - part1 - after Vladimir's remarks

--- multyThreading/src/telran/multithreading/games/Race.java
+++ multyThreading/src/telran/multithreading/games/Race.java
@@ -7,25 +7,21 @@ public class Race {
 	private int minSleep;
 	private int maxSleep;
 	//private int winner = -1;
-	private static AtomicInteger winnerID = new AtomicInteger(-1);
+	private static AtomicInteger winnerID = new AtomicInteger();
 	public Race(int distance, int minSleep, int maxSleep) {
 		this.distance = distance;
 		this.minSleep = minSleep;
 		this.maxSleep = maxSleep;
+		winnerID.set(-1);
 	}
 	public int getWinner() {
 		//return winner;
 		return winnerID.get();
 	}
 	public void setWinner(int winner) {
-		/*
-		if (this.winner == -1) {
-			this.winner = winner;
-			
-		}
-		*/
-		winnerID.compareAndSet(winnerID.get(), winner);
- 
+	
+		//winnerID.compareAndSet(winnerID.get(), winner);
+ Race.winnerID.compareAndSet(Race.winnerID.get(), winner);
 	}
 	public int getDistance() {
 		return distance;
